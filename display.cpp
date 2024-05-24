0399b67249696b2c11dbf9f7fc9e7b74a84aee52
sleepycreamcake <younglaiengchi@gmail.com>
2024-05-24 11:35:56 -0700

    Create display.cpp and display.h for AdminDisplay

---
+++ display.cpp
@@ -0,0 +1,10 @@
+#include "display.h"
+
+void Display::displayAdmin() {
+    cout << "Welcome To Our Library Management System!!!" << endl << endl;
+    cout << "What would you like to do?" << endl << endl;
+
+    cout << "> Add Book(a)" << endl;
+    cout << "> Remove Book(r)" << endl;
+    cout << "> Edit Book(e)" << endl;
+}
---
+++ display.h
@@ -0,0 +1,15 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Display {
+    public:
+        void displayAdmin();
+        
+};
+
+#endif

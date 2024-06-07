48a454557aad08a999f4e5f6ee01771cbd460f67
Rishi Dave <rishipdave@gmail.com>
2024-06-06 23:54:14 -0700

    trying to fix gtest

---
+++ src/CMakeFiles/simpleLibrary.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simpleLibrary.dir/Book.cpp.o"
+  "CMakeFiles/simpleLibrary.dir/Book.cpp.o.d"
+  "libsimpleLibrary.a"
+  "libsimpleLibrary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simpleLibrary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
